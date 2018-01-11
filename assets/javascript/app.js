7b8c2f7156881ea13c2ab70f6aef55e6de99ad24
Jeff DuBois <jeff.adubois@gmail.com>
2018-01-11 02:40:35 -0600

    update - fix missing &lt; tag

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -87,6 +87,7 @@ $(document).ready(function() {
 
   //Animate or make static.
   $(".gif").on("click", function() {
+	   console.log(".gif on click");
       // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
       var state = $(this).attr("data-state");
       // If the clicked image's state is still, update its src attribute to what its data-animate value is.
